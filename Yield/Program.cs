c673b98a2fcba9bd1439d2e47c0f30cfefc2b2bb
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-05-24 13:46:34 +0200

    Yield example

--- Yield/Program.cs
+++ Yield/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Yield
 {
@@ -6,7 +8,62 @@ namespace Yield
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foreach (int item in GetNumbers())
+            {
+                Console.WriteLine(item);
+            }
+
+            var numbers = GetNumbers();
+            IEnumerator<int> enumerator = numbers.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                int item = enumerator.Current;
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in GetNumbersOrdinary().Take(10))
+            {
+                Console.WriteLine(item);
+            }
+            
+            foreach (var item in GetNumbersYield().Take(10))
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static IEnumerable<int> GetNumbersYield()
+        {
+            var i  = 0;
+
+            while (true)
+            {
+                yield return ++i;
+            }
+        }
+
+        private static IEnumerable<int> GetNumbersOrdinary()
+        {
+            int i = 0;
+            var result = new List<int>();
+
+            while(i < 1000000)
+            {
+                result.Add(++i);
+            }
+
+            return result;
+        }
+
+        private static IEnumerable<int> GetNumbers()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+            yield return 4;
+            yield return 5;
+            yield return 6;
         }
     }
 }
